de9c6dc161c91d1376fba5370f4ae7653f6e3bb2
Lance Fallon <fallon.lance@gmail.com>
2015-03-08 12:28:00 -0400

    return flyway instead of flywayHelper

--- src/main/java/com/lance/flywaydemo/app/Driver.java
+++ src/main/java/com/lance/flywaydemo/app/Driver.java
@@ -1,5 +1,7 @@
 package com.lance.flywaydemo.app;
 
+import org.flywaydb.core.Flyway;
+
 import com.lance.flywaydemo.models.Contact;
 
 
@@ -7,10 +9,9 @@ import com.lance.flywaydemo.models.Contact;
 public class Driver {
 
 	public static void main(String[] args) {
-		FlywayHelper flywayHelper = new FlywayHelper();
-		//flywayHelper.migrateToVersion("1.0", true);
-		flywayHelper.getFlyway().setTarget("1.0");
-		flywayHelper.getFlyway().migrate();
+		Flyway flyway = new FlywayHelper().getFlyway();
+		flyway.setTarget("1.0");
+		flyway.migrate();
 		DbUtil db = new DbUtil();
 		for (Contact c : db.getContactDao().getAll()) {
 			System.out.println(c.toString());
