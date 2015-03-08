5508c894713ce530b32f90923e59412d6df7f748
Lance Fallon <fallon.lance@gmail.com>
2015-03-08 16:39:12 -0400

    remove flyway clean from master

--- src/main/java/com/lance/flywaydemo/app/Driver.java
+++ src/main/java/com/lance/flywaydemo/app/Driver.java
@@ -11,7 +11,6 @@ public class Driver {
 
 	public static void main(String[] args) {
 		Flyway flyway = new FlywayHelper().getFlyway();
-		flyway.clean();
 		flyway.setTarget("1.2");
 		flyway.migrate();
 		DbUtil db = new DbUtil();
